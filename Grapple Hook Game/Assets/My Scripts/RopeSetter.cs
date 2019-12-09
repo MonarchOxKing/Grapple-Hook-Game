e667d45075abca871ad342602c0771d534899ded
Ox King <MonarchOxKing@gmail.com>
2019-12-09 12:10:50 +1100

    Continued work on Rope Scripts
    Gained Rope Pull functionality

---
+++ Grapple Hook Game/Assets/My Scripts/RopeSetter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RopeSetter : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public SpiderRope rope;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Vector2 worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            rope.setStart(worldPos);
+        }
+    }
+}
--- Grapple Hook Game/Assets/My Scripts/SpiderRope.cs
+++ Grapple Hook Game/Assets/My Scripts/SpiderRope.cs
@@ -8,8 +8,16 @@ public class SpiderRope : MonoBehaviour
 
     private LineRenderer line;
 
+    public Material mat;
     public Rigidbody2D origin;
     public float line_width = .1f;
+    public float speed = 75;
+    public float pull_force = 50;
+
+    private Vector3 velocity;
+
+    private bool update = false;
+    private bool pull = false;
     void Start()
     {
         line = GetComponent<LineRenderer>();
@@ -19,13 +27,52 @@ public class SpiderRope : MonoBehaviour
         }
         line.startWidth = line_width;
         line.endWidth = line_width;
+        line.material = mat;
 
+    }
+
+    public void setStart(Vector2 targetPos)
+    {
+        Vector2 dir = targetPos - origin.position;
+        dir = dir.normalized;
+        velocity = dir * speed;
+        transform.position = origin.position + dir;
+        pull = false;
+        update = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!update)
+        {
+            return;
+        }
+
+        if (pull)
+        {
+            Vector2 dir = (Vector2)transform.position - origin.position;
+            origin.AddForce(dir * pull_force);
+        }
+        else
+        {
+            transform.position += velocity * Time.deltaTime;
+            float distance = Vector2.Distance(transform.position, origin.position);
+            if (distance > 50)
+            {
+                update = false;
+                line.SetPosition(0, Vector2.zero);
+                line.SetPosition(1, Vector2.zero);
+                return;
+            }
+        }
         line.SetPosition(0, transform.position);
         line.SetPosition(1, origin.position);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        velocity = Vector2.zero;
+        pull = true;
     }
 }
