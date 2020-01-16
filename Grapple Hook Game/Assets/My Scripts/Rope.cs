d5e7d0bf338482afb3ea0c075ce60338922f578f
Ox King <MonarchOxKing@gmail.com>
2020-01-16 11:29:08 +1100

    Updates to Player and Rope mechanics

---
+++ Grapple Hook Game/Assets/My Scripts/Rope.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rope : MonoBehaviour
+{
+    private LineRenderer lineRenderer;
+    public Transform RopeHit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.enabled = false;
+        lineRenderer.useWorldSpace = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.position);
+        Debug.DrawLine(transform.position, hit.point);
+        RopeHit.position = hit.point;
+        lineRenderer.SetPosition(0, transform.position);
+        lineRenderer.SetPosition(1, RopeHit.position);
+       // if(Input.GetKey(KeyCode.Space))
+        //{
+        //    lineRenderer.enabled = true;
+        //}
+       // else
+        //{
+       //     lineRenderer.enabled = false;
+       // }
+    }
+}
